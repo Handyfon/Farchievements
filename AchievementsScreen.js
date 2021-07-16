248bdaf3084cc2016b55ec8ce878647c4ad4b465
Handyfon <Handyfon@hotmail.com>
2021-07-16 10:28:06 +0200

    1.4

--- AchievementsScreen.js
+++ AchievementsScreen.js
@@ -34,6 +34,14 @@ Hooks.once('init', function() {
         default: "Your Achievements",
         type: String,
     });
+	game.settings.register('farchievements', 'HideUnknown', {
+        name: 'Hide Unknown Achievements',
+        hint: 'Completely hide Unknown Achievements from your players',
+        scope: 'world',
+        config: true,
+        default: false,
+        type: Boolean,
+    });
 	game.settings.register('farchievements', 'UnknownName', {
         name: 'Unknown Achievement',
         hint: 'Standard name of unknown Achievement (leave empty to disable)',
@@ -119,7 +127,7 @@ Hooks.once('init', function() {
         hint: 'The data for the achievements is saved here !!DONT TOUCH!!',
         scope: 'world',
         config: false,
-        default: "1:::Mounted////systems/dnd5e/icons/items/inventory/horseshoe.jpg////Acquire a mount.;;;2:::Translator////systems/dnd5e/icons/items/inventory/note-scroll.jpg////Act as the party translator.;;;3:::Argumenter////systems/dnd5e/icons/items/inventory/monster-beak.jpg////Argue with the DM over a dice roll.;;;4:::Bitte, Bitte Papa////systems/dnd5e/icons/items/inventory/runestone-dwarven.jpg////Ask a deity for a favor.;;;5:::Oblivious////icons/skills/wounds/injury-eyes-blood-red-pink.webp////Be deaf and blind simultaneously.;;;6:::You have no power here////systems/dnd5e/icons/skills/blood_12.jpg////Be ignored by the DM when citing rules.;;;7:::Special////systems/dnd5e/icons/skills/green_27.jpg////Be the only person to roll 20 at a session;;;8:::Actor////systems/dnd5e/icons/skills/emerald_07.jpg////Beat a performance check while in disguise;;;9:::Deiety////systems/dnd5e/icons/skills/yellow_13.jpg////Become deified.;;;10:::Brute////icons/magic/earth/barrier-stone-brown-green.webp////Burst through a wall.;;;11:::Ouch////https://assets.forge-vtt.com/5fa2d7054f8a4cf1b34c8a38/Icons/spellbook_page1/SpellBook08_13.png////Reach 0 HP twice in 1 encounter.;;;12:::Amazing Roleplayer////icons/skills/social/diplomacy-peace-alliance.webp////Roleplay your character exceptionally.;;;13:::(Un)advantage////icons/magic/control/voodoo-doll-pain-damage-purple.webp////Roll 2 1’s on an advantaged roll.;;;14:::Lucky////icons/magic/light/projectile-flare-blue.webp////Roll 2 20’s in a row.;;;15:::Never tell me the odds////icons/magic/control/buff-luck-fortune-clover-green.webp////Roll 2 20’s on a disadvantaged roll.;;;16:::Strongest in the Land////icons/skills/melee/unarmed-punch-fist.webp////Have a strength score over 20.;;;17:::Fastest in the Land////icons/magic/lightning/bolt-strike-cloud-gray.webp////Have a dexterity score over 20.;;;18:::Toughest in the Land////icons/magic/earth/strike-fist-stone-light.webp////Have a constitution score over 20.;;;19:::Smartest in the Land////icons/magic/control/silhouette-hold-beam-blue.webp////Have a intelligence score over 20.;;;20:::Wisest in the Land////icons/magic/nature/tree-elm-roots-brown.webp////Have a wisdom score over 20.;;;21:::The most Charming in the Land////icons/magic/unholy/strike-body-explode-disintegrate.webp////Have a charisma score over 20.;;;22:::I've nothing left to lose...////icons/magic/death/undead-skeleton-deformed-red.webp////...so the only path to choose is twisted                                Be the sole survivor of a TPK;;;23:::Necromancer////icons/commodities/bones/bones-dragon-grey.webp////Raise the dead.;;;",
+        default: "1:::Mounted////systems/dnd5e/icons/items/inventory/horseshoe.jpg////Acquire a mount.;;;2:::Translator////systems/dnd5e/icons/items/inventory/note-scroll.jpg////Act as the party translator.;;;3:::Argumenter////systems/dnd5e/icons/items/inventory/monster-beak.jpg////Argue with the DM over a dice roll.;;;4:::Bitte, Bitte Papa////systems/dnd5e/icons/items/inventory/runestone-dwarven.jpg////Ask a deity for a favor.;;;5:::Hardmode////icons/skills/wounds/injury-eyes-blood-red-pink.webp////Be deaf and blind simultaneously.;;;6:::You have no power here////systems/dnd5e/icons/skills/blood_12.jpg////Be ignored by the DM when citing rules.;;;7:::Special////systems/dnd5e/icons/skills/green_27.jpg////Be the only person to roll 20 at a session;;;8:::Actor////systems/dnd5e/icons/skills/emerald_07.jpg////Beat a performance check while in disguise;;;9:::Deiety////systems/dnd5e/icons/skills/yellow_13.jpg////Become deified.;;;10:::Brute////icons/magic/earth/barrier-stone-brown-green.webp////Burst through a wall.;;;11:::Ouch////https://assets.forge-vtt.com/5fa2d7054f8a4cf1b34c8a38/Icons/spellbook_page1/SpellBook08_13.png////Reach 0 HP twice in 1 encounter.;;;12:::Amazing Roleplayer////icons/skills/social/diplomacy-peace-alliance.webp////Roleplay your character exceptionally.;;;13:::(Un)advantage////icons/magic/control/voodoo-doll-pain-damage-purple.webp////Roll 2 1’s on an advantaged roll.;;;14:::Lucky////icons/magic/light/projectile-flare-blue.webp////Roll 2 20’s in a row.;;;15:::Never tell me the odds////icons/magic/control/buff-luck-fortune-clover-green.webp////Roll 2 20’s on a disadvantaged roll.;;;16:::Strongest in the Land////icons/skills/melee/unarmed-punch-fist.webp////Have a strength score over 20.;;;17:::Fastest in the Land////icons/magic/lightning/bolt-strike-cloud-gray.webp////Have a dexterity score over 20.;;;18:::Toughest in the Land////icons/magic/earth/strike-fist-stone-light.webp////Have a constitution score over 20.;;;19:::Smartest in the Land////icons/magic/control/silhouette-hold-beam-blue.webp////Have a intelligence score over 20.;;;20:::Wisest in the Land////icons/magic/nature/tree-elm-roots-brown.webp////Have a wisdom score over 20.;;;21:::The most Charming in the Land////icons/magic/unholy/strike-body-explode-disintegrate.webp////Have a charisma score over 20.;;;22:::I've nothing left to lose...////icons/magic/death/undead-skeleton-deformed-red.webp////...so the only path to choose is twisted                                Be the sole survivor of a TPK;;;23:::Necromancer////icons/commodities/bones/bones-dragon-grey.webp////Raise the dead.;;;",
         type: String,
     });
 	game.settings.register('farchievements', 'clientdataSYNC', {
@@ -159,6 +167,7 @@ class Achievements {
             const chatControlLeftNode = chatControlLeft.firstElementChild;
             const number = 3;
             tableNode = document.createElement("label");
+			tableNode.className = "AchievmentButtonLabel";
             tableNode.innerHTML = `<i id="achievements-button" class="fas fa-medal achievements-button" style="text-shadow: 0 0 1px black;"></i>`;
             tableNode.onclick = Achievements.initializeAchievements;
             chatControlLeft.insertBefore(tableNode, chatControlLeftNode);
@@ -264,7 +273,7 @@ class AchievementSync{
 				}
 			}
 			else{
-				for(let i = 1; i < game.users._source.length; i++){
+				for(let i = 0; i < game.users._source.length; i++){
 					if(!clientDataSYNC.includes(game.users._source[i]._id)){ //IF A NEW USER IS DETECTED ADD HIM TO THE SYNC SETTING
 						clientDataSYNC += game.users._source[i]._id + ":||"
 						//ui.notifications.notify("Foundry Achievements | Added "+game.users._source[i].name+" with ID: " + game.users._source[i]._id);
@@ -274,12 +283,17 @@ class AchievementSync{
 				//CHECK FOR REDUNDANT USERS AND REMOVE THEM
 				let userID;
 				let ToSYNC = clientDataSYNC.split('||');
-				for(let i = 1; clientDataSYNC.split('||').length > game.users._source.length; i++){
+				if(game.users._source.length == 1){
+					//ui.notifications.notify("Farchievements | you need to create some players in order to use this module");
+					return;
+				}
+				for(let i = 0; i < clientDataSYNC.split('||').length; i++){//SKIP GM
 					userID = clientDataSYNC.split('||')[i].split(":")[0];
-					if(game.users.find(user => user.id == userID) == null){ //IF A NEW USER IS DETECTED ADD HIM TO THE SYNC SETTING
-						ToSYNC.pop(i);
+					if(game.users.get(userID) == null){ //IF A NEW USER IS DETECTED ADD HIM TO THE SYNC SETTING
+						//ToSYNC.pop(i);
+						ToSYNC.splice(i, 1);
 						clientDataSYNC = ToSYNC.join("||") + "||";
-						//console.log(clientDataSYNC);
+						console.log(clientDataSYNC);
 						if(userID != "")
 						ui.notifications.notify("Foundry Achievements | Player ID's Changed, purged Player with ID: " + userID);
 					}
@@ -326,6 +340,7 @@ Hooks.on('renderSceneNavigation', function() {
         Achievements.addChatControl();
         //console.log("AchievementsScreen GM true");
 		//sync achievements
+		if(!game.user.isGM)
 		AchievementSync.SyncAchievements();
 		let bannerstyle = 'top: -200px;background: url('+game.settings.get("farchievements", "bannerBackground")+')!important;background-size: cover !important;background-position: center !important;box-shadow: 0px -4px 6px black !important;display: flex;';
 		var el = '<div id="Achievementbar" style="display: none;" class="Achievementbar"><div id="FoundryAchievements" class="FoundryAchievementsBanner" style="'+bannerstyle+'"><img id="AchievementIMG" class="AchievementIMG" src="modules/farchievements/standardIcon.PNG"></img><p class="AchievementText"><label class="AchievementTextLabel">New Achievement:</label> (Achievement) </p><i class="Shiny"></i></div></div>';
@@ -333,17 +348,17 @@ Hooks.on('renderSceneNavigation', function() {
 	
 });
 
-
-Hooks.on('renderSceneDirectory', function() {
+Hooks.on('renderSettings', function() {
 	//ADD BUTTON TO SETTINGS
-	if(document.getElementById("SettingsAchievementsButton") == null && game.settings.get('farchievements', 'GameSettingsButton')){
-		$('#settings-game').append('<button id="SettingsAchievementsButton" data-action="Achievements"><i class="fas fa-medal achievements-button"></i>Achievements</button>');
-		let AchievementsButton = document.getElementById("SettingsAchievementsButton");
-		if(AchievementsButton != null)
-		AchievementsButton.onclick = Achievements.initializeAchievements;
-	}
 	function refreshData(){
 		let x = 0.1;  // 0.1 seconds
+		
+		if(document.getElementById("FarchievementsSettings") == null && game.settings.get('farchievements', 'GameSettingsButton')){
+			$('#settings-game').append('<div id="FarchievementsSettings"><h4>Farchievements</h4><button id="SettingsAchievementsButton" data-action="Achievements"><i class="fas fa-medal achievements-button"></i>Achievements</button></div>');
+			let AchievementsButton = document.getElementById("SettingsAchievementsButton");
+			if(AchievementsButton != null)
+			AchievementsButton.onclick = Achievements.initializeAchievements;
+		}
 		// Do your thing here
 		//ui.notifications.notify("Test");
 		if(document.getElementsByClassName("context-items")[0] != null){
@@ -372,6 +387,7 @@ Hooks.on('renderSceneDirectory', function() {
 		}
 		WaitForReady();
 	}
+	
 	refreshData();
 
 });
