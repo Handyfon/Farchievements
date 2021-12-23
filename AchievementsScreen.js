3a629b8e6dc72cfab8d7e0824de77abce46a161f
Handyfon <Handyfon@hotmail.com>
2021-12-23 10:01:57 +0100

    1.6 for v9

--- AchievementsScreen.js
+++ AchievementsScreen.js
@@ -8,6 +8,31 @@ Hooks.once('init', function() {
         default: false,
         type: Boolean,
 		onChange: debouncedReload,
+	});
+	game.settings.register('farchievements', 'EnableAchievementMessage', {
+        name: 'Enable Chatmessage',
+        hint: 'Posts a message when a player gained an achievement.',
+        scope: 'world',
+        config: true,
+        default: false,
+        type: Boolean,
+	});
+	game.settings.register('farchievements', 'EnableScoreboard', {
+        name: 'Enable Scoreboard',
+        hint: 'Enables Scoreboard for players.',
+        scope: 'world',
+        config: true,
+        default: true,
+        type: Boolean,
+	});
+	if (game.modules.get('confetti')?.active === true)
+	game.settings.register('farchievements', 'EnableConfettiSupport', {
+        name: 'Enable Confetti',
+		hint: 'Enable Support for the Confetti module',
+        scope: 'world',
+        config: true,
+        default: true,
+        type: Boolean,
     });
 	game.settings.register('farchievements', 'EnableContextButton', {
         name: 'Enable the button for the player-list context menu',
@@ -22,8 +47,8 @@ Hooks.once('init', function() {
         hint: 'OmniView',
         scope: 'world',
         config: true,
-        default: false,
-        type: Boolean,
+		default: false,
+		type: Boolean,
     });
 	game.settings.register('farchievements', 'GameSettingsButton', {
         name: 'Game Settings-Button',
@@ -112,7 +137,8 @@ Hooks.once('init', function() {
         scope: 'world',
         config: true,
         default: "modules/farchievements/standardIcon.PNG",
-        type: String,
+		type: String,
+		filePicker: 'image',
     });
 	game.settings.register('farchievements', 'bannerBackground', {
         name: 'Banner background',
@@ -120,15 +146,30 @@ Hooks.once('init', function() {
         scope: 'world',
         config: true,
         default: "modules/farchievements/achievementbanner.jpg",
-        type: String,
+		type: String,
+		filePicker: 'image',
     });
 	game.settings.register('farchievements', 'achievementStinger', {
-        name: 'Achievement Stinger SFX',
-        hint: 'Background for the achievement banner',
+        name: 'Achievement Stinger',
+		hint: 'Audio that will be played during the animation',
         scope: 'world',
         config: true,
         default: "modules/farchievements/standardStinger_by_JFarenheit.mp3",
-        type: String,
+		type: String,
+		filePicker: 'audio',
+	});
+	game.settings.register('farchievements', 'achievementStingerVolume', {
+        name: 'Achievement Stinger Volume',
+        hint: 'This is the volume all stingers will be played on, I recommend to keep it low',
+        scope: 'world',
+        config: true,
+        default: "0.1",
+		type: Number,
+		range: {
+			min: 0,
+			max: 1,
+			step: 0.01
+		}
     });
 	game.settings.register('farchievements', 'achpretext', {
         name: 'New Achievement Prefix',
@@ -152,21 +193,22 @@ Hooks.once('init', function() {
         scope: 'world',
         config: true,
         default: "modules/farchievements/mystery.JPG",
-        type: String,
+		type: String,
+		filePicker: 'image',
     });
 	game.settings.register('farchievements', 'achievementdata', {
         name: 'AchievementData (DONTTOUCH)',
         hint: 'The data for the achievements is saved here !!DONT TOUCH!!',
         scope: 'world',
-        config: false,
-        default: "1:::Mounted////systems/dnd5e/icons/items/inventory/horseshoe.jpg////Acquire a mount.;;;2:::Translator////systems/dnd5e/icons/items/inventory/note-scroll.jpg////Act as the party translator.;;;3:::Argumenter////systems/dnd5e/icons/items/inventory/monster-beak.jpg////Argue with the DM over a dice roll.;;;4:::Bitte, Bitte Papa////systems/dnd5e/icons/items/inventory/runestone-dwarven.jpg////Ask a deity for a favor.;;;5:::Hardmode////icons/skills/wounds/injury-eyes-blood-red-pink.webp////Be deaf and blind simultaneously.;;;6:::You have no power here////systems/dnd5e/icons/skills/blood_12.jpg////Be ignored by the DM when citing rules.;;;7:::Special////systems/dnd5e/icons/skills/green_27.jpg////Be the only person to roll 20 at a session;;;8:::Actor////systems/dnd5e/icons/skills/emerald_07.jpg////Beat a performance check while in disguise;;;9:::Deiety////systems/dnd5e/icons/skills/yellow_13.jpg////Become deified.;;;10:::Brute////icons/magic/earth/barrier-stone-brown-green.webp////Burst through a wall.;;;11:::Ouch////https://assets.forge-vtt.com/5fa2d7054f8a4cf1b34c8a38/Icons/spellbook_page1/SpellBook08_13.png////Reach 0 HP twice in 1 encounter.;;;12:::Amazing Roleplayer////icons/skills/social/diplomacy-peace-alliance.webp////Roleplay your character exceptionally.;;;13:::(Un)advantage////icons/magic/control/voodoo-doll-pain-damage-purple.webp////Roll 2 1’s on an advantaged roll.;;;14:::Lucky////icons/magic/light/projectile-flare-blue.webp////Roll 2 20’s in a row.;;;15:::Never tell me the odds////icons/magic/control/buff-luck-fortune-clover-green.webp////Roll 2 20’s on a disadvantaged roll.;;;16:::Strongest in the Land////icons/skills/melee/unarmed-punch-fist.webp////Have a strength score over 20.;;;17:::Fastest in the Land////icons/magic/lightning/bolt-strike-cloud-gray.webp////Have a dexterity score over 20.;;;18:::Toughest in the Land////icons/magic/earth/strike-fist-stone-light.webp////Have a constitution score over 20.;;;19:::Smartest in the Land////icons/magic/control/silhouette-hold-beam-blue.webp////Have a intelligence score over 20.;;;20:::Wisest in the Land////icons/magic/nature/tree-elm-roots-brown.webp////Have a wisdom score over 20.;;;21:::The most Charming in the Land////icons/magic/unholy/strike-body-explode-disintegrate.webp////Have a charisma score over 20.;;;22:::I've nothing left to lose...////icons/magic/death/undead-skeleton-deformed-red.webp////...so the only path to choose is twisted                                Be the sole survivor of a TPK;;;23:::Necromancer////icons/commodities/bones/bones-dragon-grey.webp////Raise the dead.;;;",
+        config: true,
+		default: "1:::Mounted////systems/dnd5e/icons/items/inventory/horseshoe.jpg////Acquire a mount.;;;2:::Translator////systems/dnd5e/icons/items/inventory/note-scroll.jpg////Act as the party translator.;;;3:::Argumenter////systems/dnd5e/icons/items/inventory/monster-beak.jpg////Argue with the DM over a dice roll.;;;4:::Bitte, Bitte Papa////systems/dnd5e/icons/items/inventory/runestone-dwarven.jpg////Ask a deity for a favor.;;;5:::Hardmode////icons/skills/wounds/injury-eyes-blood-red-pink.webp////Be deaf and blind simultaneously.;;;6:::You have no power here////systems/dnd5e/icons/skills/blood_12.jpg////Be ignored by the DM when citing rules.;;;7:::Special////systems/dnd5e/icons/skills/green_27.jpg////Be the only person to roll 20 at a session;;;8:::Actor////systems/dnd5e/icons/skills/emerald_07.jpg////Beat a performance check while in disguise;;;9:::Deiety////systems/dnd5e/icons/skills/yellow_13.jpg////Become deified.;;;10:::Brute////icons/magic/earth/barrier-stone-brown-green.webp////Burst through a wall.;;;11:::Ouch////https://assets.forge-vtt.com/5fa2d7054f8a4cf1b34c8a38/Icons/spellbook_page1/SpellBook08_13.png////Reach 0 HP twice in 1 encounter.;;;12:::Amazing Roleplayer////icons/skills/social/diplomacy-peace-alliance.webp////Roleplay your character exceptionally.;;;13:::(Un)advantage////icons/magic/control/voodoo-doll-pain-damage-purple.webp////Roll 2 1’s on an advantaged roll.;;;14:::Lucky////icons/magic/light/projectile-flare-blue.webp////Roll 2 20’s in a row.;;;15:::Never tell me the odds////icons/magic/control/buff-luck-fortune-clover-green.webp////Roll 2 20’s on a disadvantaged roll.;;;16:::Strongest in the Land////icons/skills/melee/unarmed-punch-fist.webp////Have a strength score over 20.;;;17:::Fastest in the Land////icons/magic/lightning/bolt-strike-cloud-gray.webp////Have a dexterity score over 20.;;;18:::Toughest in the Land////icons/magic/earth/strike-fist-stone-light.webp////Have a constitution score over 20.;;;19:::Smartest in the Land////icons/magic/control/silhouette-hold-beam-blue.webp////Have a intelligence score over 20.;;;20:::Wisest in the Land////icons/magic/nature/tree-elm-roots-brown.webp////Have a wisdom score over 20.;;;21:::The most Charming in the Land////icons/magic/unholy/strike-body-explode-disintegrate.webp////Have a charisma score over 20.;;;22:::I've nothing left to lose...////icons/magic/death/undead-skeleton-deformed-red.webp////...so the only path to choose is twisted. Be the sole survivor of a TPK;;;23:::Necromancer////icons/commodities/bones/bones-dragon-grey.webp////Raise the dead.;;;24:::Lorax////https://c.tenor.com/BzpCcZbxOAIAAAAd/lorax-the-lorax.gif////Speak for the trees;;;",
         type: String,
     });
 	game.settings.register('farchievements', 'clientdataSYNC', {
         name: 'ClientDataList (DONTTOUCH)',
         hint: 'will be synced between clients !!DONT TOUCH!!',
         scope: 'world',
-        config: false,
+		config: true,
         default: "",
         type: String,
     });
@@ -174,7 +216,7 @@ Hooks.once('init', function() {
         name: 'ClientData (DONTTOUCH)',
         hint: 'your clients achievements !!DONT TOUCH!!',
         scope: 'client',
-        config: false,
+        config: true,
         default: "",
         type: String,
     });
@@ -266,6 +308,7 @@ class AchievementSync{
 		let data;
 		let name,icon;
 		let toGain;
+		let stingerVolume = game.settings.get('farchievements', 'achievementStingerVolume');
 		for(let i = 0; i < achievementsToGain.length; i++){
 			await AchievementSync.sleep(100);
 			toGain = achievementsToGain[i];
@@ -274,12 +317,23 @@ class AchievementSync{
 			name = data.split(":::")[1].split("////")[0];
 			icon = data.split(":::")[1].split("////")[1];
 			if(icon == "icon"){icon = game.settings.get('farchievements', 'standarticon')} //IF STANDARD ICON USE ICON DEFINED IN GAMESETTINGS
-			await AudioHelper.play({src: game.settings.get('farchievements', 'achievementStinger'), volume: 0.2, autoplay: true, loop: false}, false);
+			await AudioHelper.play({ src: game.settings.get('farchievements', 'achievementStinger'), volume: stingerVolume, autoplay: true, loop: false}, false);
 			await AchievementSync.sleep(1800);
 			document.getElementsByClassName("AchievementText")[0].innerHTML = '<label class="AchievementTextLabel">'+game.settings.get("farchievements", "achpretext")+'</label>' + name;
 			document.getElementById("AchievementIMG").src = icon;
 			document.getElementById("Achievementbar").style.setProperty("display", "flex");
-			if(game.modules.get('confetti')?.active === true){
+			if (game.settings.get('farchievements', 'EnableAchievementMessage')){
+
+				let tempUserName = game.user.name;
+				ChatMessage.create({
+					user: game.user.id,
+					content: tempUserName + " gained the '" + name +"' achievement",
+					blind: false,
+				});
+            }
+
+
+			if (game.modules.get('confetti')?.active === true && game.settings.get('farchievements', 'EnableConfettiSupport')){
 				for(let c = 0; c <3; c++){
 					await AchievementSync.sleep(500);
 					const strength = window.confetti.confettiStrength.high;
@@ -476,6 +530,7 @@ if(message.data.content.includes("Farchievements-SyncRequest")){
 		blind: false,
 		whisper : game.users.entities.filter(u => u.isGM).map(u => u.id)
 	});
+	ui.notifications.notify("Achievements Synced");
 	AchievementSync.SyncAchievements();
 }
 });
