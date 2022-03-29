3f0cbf92cf8bc3cae6b97340fcf2b7d95fa470ff
Handyfon <Handyfon@hotmail.com>
2022-03-29 17:05:03 +0200

    1.7.3

--- AchievementsScreen.js
+++ AchievementsScreen.js
@@ -136,7 +136,7 @@ Hooks.once('init', function() {
         hint: 'Standard Icon',
         scope: 'world',
         config: true,
-        default: "modules/farchievements/standardIcon.PNG",
+        default: "modules/farchievements/standardIcon_silver.webp",
 		type: String,
 		filePicker: 'image',
     });
@@ -200,7 +200,7 @@ Hooks.once('init', function() {
         name: 'AchievementData (DONTTOUCH)',
         hint: 'The data for the achievements is saved here !!DONT TOUCH!!',
         scope: 'world',
-		config: false,
+		config: false, // DISABLE
 		default: "1:::Mounted////systems/dnd5e/icons/items/inventory/horseshoe.jpg////Acquire a mount.;;;2:::Translator////systems/dnd5e/icons/items/inventory/note-scroll.jpg////Act as the party translator.;;;3:::Argumenter////systems/dnd5e/icons/items/inventory/monster-beak.jpg////Argue with the DM over a dice roll.;;;4:::Bitte, Bitte Papa////systems/dnd5e/icons/items/inventory/runestone-dwarven.jpg////Ask a deity for a favor.;;;5:::Hardmode////icons/skills/wounds/injury-eyes-blood-red-pink.webp////Be deaf and blind simultaneously.;;;6:::You have no power here////systems/dnd5e/icons/skills/blood_12.jpg////Be ignored by the DM when citing rules.;;;7:::Special////systems/dnd5e/icons/skills/green_27.jpg////Be the only person to roll 20 at a session;;;8:::Actor////systems/dnd5e/icons/skills/emerald_07.jpg////Beat a performance check while in disguise;;;9:::Deiety////systems/dnd5e/icons/skills/yellow_13.jpg////Become deified.;;;10:::Brute////icons/magic/earth/barrier-stone-brown-green.webp////Burst through a wall.;;;11:::Ouch////https://assets.forge-vtt.com/5fa2d7054f8a4cf1b34c8a38/Icons/spellbook_page1/SpellBook08_13.png////Reach 0 HP twice in 1 encounter.;;;12:::Amazing Roleplayer////icons/skills/social/diplomacy-peace-alliance.webp////Roleplay your character exceptionally.;;;13:::(Un)advantage////icons/magic/control/voodoo-doll-pain-damage-purple.webp////Roll 2 1’s on an advantaged roll.;;;14:::Lucky////icons/magic/light/projectile-flare-blue.webp////Roll 2 20’s in a row.;;;15:::Never tell me the odds////icons/magic/control/buff-luck-fortune-clover-green.webp////Roll 2 20’s on a disadvantaged roll.;;;16:::Strongest in the Land////icons/skills/melee/unarmed-punch-fist.webp////Have a strength score over 20.;;;17:::Fastest in the Land////icons/magic/lightning/bolt-strike-cloud-gray.webp////Have a dexterity score over 20.;;;18:::Toughest in the Land////icons/magic/earth/strike-fist-stone-light.webp////Have a constitution score over 20.;;;19:::Smartest in the Land////icons/magic/control/silhouette-hold-beam-blue.webp////Have a intelligence score over 20.;;;20:::Wisest in the Land////icons/magic/nature/tree-elm-roots-brown.webp////Have a wisdom score over 20.;;;21:::The most Charming in the Land////icons/magic/unholy/strike-body-explode-disintegrate.webp////Have a charisma score over 20.;;;22:::I've nothing left to lose...////icons/magic/death/undead-skeleton-deformed-red.webp////...so the only path to choose is twisted. Be the sole survivor of a TPK;;;23:::Necromancer////icons/commodities/bones/bones-dragon-grey.webp////Raise the dead.;;;24:::Lorax////https://c.tenor.com/BzpCcZbxOAIAAAAd/lorax-the-lorax.gif////Speak for the trees;;;",
         type: String,
     });
@@ -437,10 +437,8 @@ Hooks.on('renderSceneNavigation', function() {
 		AchievementSync.SyncAchievements();
 		let bannerstyle = 'top: -200px;background: url('+game.settings.get("farchievements", "bannerBackground")+')!important;background-size: cover !important;background-position: center !important;box-shadow: 0px -4px 6px black !important;display: flex;';
 		var el = '<div id="Achievementbar" style="display: none;" class="Achievementbar"><div id="FoundryAchievements" class="FoundryAchievementsBanner" style="'+bannerstyle+'"><img id="AchievementIMG" class="AchievementIMG" src="modules/farchievements/standardIcon.PNG"></img><p class="AchievementText"><label class="AchievementTextLabel">New Achievement:</label> (Achievement) </p><i class="Shiny"></i></div></div>';
-		document.getElementById("notifications").innerHTML = el;
-	
+		document.getElementById("notifications").innerHTML = el;	
 });
-
 Hooks.on('renderSettings', function() {
 	//ADD BUTTON TO SETTINGS
 	function refreshData(){
@@ -481,13 +479,11 @@ Hooks.on('renderSettings', function() {
 		WaitForReady();
 	}
 	
-	refreshData();
-
-});
+	refreshData();});
 Hooks.on("createChatMessage", async function (message){
 	if(message.data.content.includes("Achievements Synced"))
 	AchievementSync.SyncAchievements();
-if(message.data.content.includes("Farchievements-SyncRequest")){
+	if(message.data.content.includes("Farchievements-SyncRequest")){
 	if(!game.user.isGM)return;
 	let NAME = message.data.content.split("|")[1];
 	let ACHIEVMENTNAME = message.data.content.split("|")[2];
@@ -532,5 +528,240 @@ if(message.data.content.includes("Farchievements-SyncRequest")){
 	});
 	ui.notifications.notify("Achievements Synced");
 	AchievementSync.SyncAchievements();
+}});
+
+window.farchievements_DEBUG_Reset_EVERYTHING = async function resetSettings(){
+	if(!game.user.isGM) return;
+	await game.settings.set('farchievements', 'achievementdata', "1:::Mounted////systems/dnd5e/icons/items/inventory/horseshoe.jpg////Acquire a mount.;;;2:::Translator////systems/dnd5e/icons/items/inventory/note-scroll.jpg////Act as the party translator.;;;3:::Argumenter////systems/dnd5e/icons/items/inventory/monster-beak.jpg////Argue with the DM over a dice roll.;;;4:::Bitte, Bitte Papa////systems/dnd5e/icons/items/inventory/runestone-dwarven.jpg////Ask a deity for a favor.;;;5:::Hardmode////icons/skills/wounds/injury-eyes-blood-red-pink.webp////Be deaf and blind simultaneously.;;;6:::You have no power here////systems/dnd5e/icons/skills/blood_12.jpg////Be ignored by the DM when citing rules.;;;7:::Special////systems/dnd5e/icons/skills/green_27.jpg////Be the only person to roll 20 at a session;;;8:::Actor////systems/dnd5e/icons/skills/emerald_07.jpg////Beat a performance check while in disguise;;;9:::Deiety////systems/dnd5e/icons/skills/yellow_13.jpg////Become deified.;;;10:::Brute////icons/magic/earth/barrier-stone-brown-green.webp////Burst through a wall.;;;11:::Ouch////https://assets.forge-vtt.com/5fa2d7054f8a4cf1b34c8a38/Icons/spellbook_page1/SpellBook08_13.png////Reach 0 HP twice in 1 encounter.;;;12:::Amazing Roleplayer////icons/skills/social/diplomacy-peace-alliance.webp////Roleplay your character exceptionally.;;;13:::(Un)advantage////icons/magic/control/voodoo-doll-pain-damage-purple.webp////Roll 2 1’s on an advantaged roll.;;;14:::Lucky////icons/magic/light/projectile-flare-blue.webp////Roll 2 20’s in a row.;;;15:::Never tell me the odds////icons/magic/control/buff-luck-fortune-clover-green.webp////Roll 2 20’s on a disadvantaged roll.;;;16:::Strongest in the Land////icons/skills/melee/unarmed-punch-fist.webp////Have a strength score over 20.;;;17:::Fastest in the Land////icons/magic/lightning/bolt-strike-cloud-gray.webp////Have a dexterity score over 20.;;;18:::Toughest in the Land////icons/magic/earth/strike-fist-stone-light.webp////Have a constitution score over 20.;;;19:::Smartest in the Land////icons/magic/control/silhouette-hold-beam-blue.webp////Have a intelligence score over 20.;;;20:::Wisest in the Land////icons/magic/nature/tree-elm-roots-brown.webp////Have a wisdom score over 20.;;;21:::The most Charming in the Land////icons/magic/unholy/strike-body-explode-disintegrate.webp////Have a charisma score over 20.;;;22:::I've nothing left to lose...////icons/magic/death/undead-skeleton-deformed-red.webp////...so the only path to choose is twisted. Be the sole survivor of a TPK;;;23:::Necromancer////icons/commodities/bones/bones-dragon-grey.webp////Raise the dead.;;;24:::Lorax////https://c.tenor.com/BzpCcZbxOAIAAAAd/lorax-the-lorax.gif////Speak for the trees;;;");
+	await game.settings.set('farchievements', 'clientdataSYNC',"");
+	await game.settings.set('farchievements', 'clientdata', "");
 }
-});
+window.farchievements_DEBUG_Reset_PlayerAchievements = async function resetPlayers(){
+	if(!game.user.isGM) return;
+	await game.settings.set('farchievements', 'clientdataSYNC',"");
+	await game.settings.set('farchievements', 'clientdata', "");
+	location.reload();
+}
+
+//PUBLICLY ACCESSIBLE FUNCTIONS		
+window.Farchievements = class Farchievement{
+	static Open (){
+		$("#SettingsAchievementsButton").click()
+	}
+	static Render (){
+		$("#SettingsAchievementsButton").click()
+	}
+	static async AddAchievement(AchievementName, PlayerName){
+		if(!game.user.isGM) return;
+		console.log(AchievementName);
+		let data = game.settings.get('farchievements', 'achievementdata').split(';;;');
+		let AchievementID, PlayerID;
+		for(let i = 0; i < game.settings.get('farchievements', 'achievementdata').split(';;;').length; i++){
+			if(AchievementName == game.settings.get('farchievements', 'achievementdata').split(';;;')[i].split('////')[0].split(":::")[1]){
+				AchievementID = game.settings.get('farchievements', 'achievementdata').split(';;;')[i].split('////')[0].split(":::")[0] - 1;
+			}
+		}
+		PlayerID = game.users.getName(PlayerName).id;
+
+		if(PlayerID == null){
+			ui.notifications.warn("Farchievements | Is the player name right?")
+			return;
+		}
+		if(AchievementID == null){
+			ui.notifications.warn("Farchievements | Is the achievement name right?")
+			return;
+		}
+		addAchievementFromCommand(AchievementID, PlayerID);
+	}
+	static async RemoveAchievement(AchievementName, PlayerName){
+		if(!game.user.isGM) return;
+		console.log(AchievementName);
+		let data = game.settings.get('farchievements', 'achievementdata').split(';;;');
+		let AchievementID, PlayerID;
+		for(let i = 0; i < game.settings.get('farchievements', 'achievementdata').split(';;;').length; i++){
+			if(AchievementName == game.settings.get('farchievements', 'achievementdata').split(';;;')[i].split('////')[0].split(":::")[1]){
+				AchievementID = game.settings.get('farchievements', 'achievementdata').split(';;;')[i].split('////')[0].split(":::")[0] - 1;
+			}
+		}
+		PlayerID = game.users.getName(PlayerName).id;
+
+		if(PlayerID == null){
+			ui.notifications.warn("Farchievements | Is the player name right?")
+			return;
+		}
+		if(AchievementID == null){
+			ui.notifications.warn("Farchievements | Is the achievement name right?")
+			return;
+		}
+		removeAchievementFromCommand(AchievementID, PlayerID);
+	}
+}
+
+async function addAchievementFromCommand(achievementID, PID) {
+			let cleanPlayerID = game.users.contents.indexOf(game.users.get(PID)) - 1;
+			let dataPlayerID = cleanPlayerID; //++xathick
+			let player = game.users.get(PID);
+			let playerName = player.name;
+			let clientdataSYNC = game.settings.get('farchievements', 'clientdataSYNC'); //GET DATA
+			let dataArray = clientdataSYNC.split("||"); //DATA TO ARRAY
+			let dataArrayPlayer; //DATA TO ARRAY
+			let toSYNC;
+			let index = 0;
+			for (index; index < dataArray.length; index++) {
+				if (dataArray[index].split(":")[0] == PID) {
+					dataPlayerID = index;
+				}
+				game.settings.get('farchievements', 'clientdataSYNC').split("||");
+			}
+			if (dataArray[dataPlayerID] == "" || dataArray[dataPlayerID] == 'NULL') { // IF NO DATA YET ADD ACHIEVEMENT
+				dataArrayPlayer = game.users.contents[dataPlayerID]._id + ":" + achievementID + ",";
+				dataArray[dataPlayerID] = dataArrayPlayer; //++xathick
+				toSYNC = dataArray.join("||");
+				await game.settings.set('farchievements', 'clientdataSYNC', toSYNC);
+				if (document.getElementById('AchPlayerNav').className == "AchPlayerNav") //CHECK FOR EDITING WITHIN NORMAL WINDOW
+				{
+					await game.settings.set('farchievements', 'loadSettingsForPlayer', PID);
+					$('#achsyncnormalmode').append('<i id="SyncAch2" onclick="SendSyncMessage()" class="fas fa-sync achievementsettings" title="Click to push changes right now"></i>');
+					loadAchievements();
+				}
+				else
+					loadAchievementsEditMode();
+				return;
+			}
+
+			if (dataArray[dataPlayerID].split(":")[1].includes(',' + "" + achievementID + ',')) { //DETECT EXISTING ACHIEVEMENT, SKIP REST
+				ui.notifications.notify("Farchievements | This player already has the achievement");
+				return;
+			}
+			else if (dataArray[dataPlayerID].split(":")[1].split(",")[0] == "" + achievementID) { //FIRST ACHIEVEMENT IN DATA? => CHECK AGAIN SPECIAL FORMATTING
+				ui.notifications.notify("Farchievements | This player already has the achievement");
+				return;
+			}
+			else if (dataArray[dataPlayerID].split(":")[1].split(",")[dataArray[dataPlayerID].split(":")[1].split(",")[0].length + 1] == "" + achievementID) { //LAST ACHIEVEMENT IN DATA? => SPECIAL FORMATTING
+				let toReplace = achievementID + ",";//REPLACE FIRST ENTRY IN DATA
+				var firstDataArray = dataArray[dataPlayerID].split(":")[1].split(",");
+				firstDataArray.pop();
+				dataArray[dataPlayerID] = dataArray[dataPlayerID].split(":")[0] + ":" + firstDataArray;
+				toSYNC = dataArray.join("||");
+			}
+			else {//IF IT DOESN'T EXIST ON THIS PLAYER YET, ADD THE ACHIEVEMENT
+				dataArrayPlayer = dataArray[dataPlayerID].split(":")[0] + ":" + dataArray[dataPlayerID].split(":")[1] + achievementID + ",";
+				dataArray[dataPlayerID] = dataArrayPlayer;
+				toSYNC = dataArray.join("||");
+			}
+			if (document.getElementById('SyncAchUnsaved') != null) {
+				if (document.getElementById('SyncAchUnsaved').value == toSYNC) {
+					document.getElementById('SyncAchUnsaved').id = "SyncAch";
+					document.getElementById('SyncAch').value = "";
+				}
+			}
+			if (document.getElementById('SyncAch') != null) {
+				document.getElementById('SyncAch').value = toSYNC;
+				document.getElementById('SyncAch').id = "SyncAchUnsaved";
+			}
+
+			await game.settings.set('farchievements', 'clientdataSYNC', toSYNC);
+			SendSyncMessage();
+			//RELOAD ANY OPEN WINDOW
+			if(document.getElementById('AchPlayerNav') == null) return;
+			if (document.getElementById('AchPlayerNav').className == "AchPlayerNav") //CHECK FOR EDITING WITHIN NORMAL WINDOW
+			{
+				await game.settings.set('farchievements', 'loadSettingsForPlayer', PID);
+				$('#achsyncnormalmode').append('<i id="SyncAch2" onclick="SendSyncMessage()" class="fas fa-sync achievementsettings" title="Click to push changes right now"></i>');
+				window.loadAchievements();
+			}
+			else
+				window.loadAchievementsEditMode();
+}
+async function removeAchievementFromCommand(achievementID, PID) {
+			let cleanPlayerID = game.users.contents.indexOf(game.users.get(PID)) - 1;
+			let dataPlayerID = cleanPlayerID; //++xathick
+			let player = game.users.get(PID);
+			let playerName = player.name;
+			let clientdataSYNC = game.settings.get('farchievements', 'clientdataSYNC'); //GET DATA
+			let dataArray = clientdataSYNC.split("||"); //DATA TO ARRAY
+			let dataArrayPlayer; //DATA TO ARRAY
+			let toSYNC;
+			let index = 0;
+			for (index; index < dataArray.length; index++) {
+				if (dataArray[index].split(":")[0] == PID) {
+					dataPlayerID = index;
+				}
+				game.settings.get('farchievements', 'clientdataSYNC').split("||");
+			}
+			if (dataArray[dataPlayerID] == "" || dataArray[dataPlayerID] == 'NULL') { // IF NO DATA YET ADD ACHIEVEMENT
+				dataArrayPlayer = game.users.contents[dataPlayerID]._id + ":" + achievementID + ",";
+				dataArray[dataPlayerID] = dataArrayPlayer; //++xathick
+				toSYNC = dataArray.join("||");
+				await game.settings.set('farchievements', 'clientdataSYNC', toSYNC);
+				if (document.getElementById('AchPlayerNav').className == "AchPlayerNav") //CHECK FOR EDITING WITHIN NORMAL WINDOW
+				{
+					await game.settings.set('farchievements', 'loadSettingsForPlayer', PID);
+					$('#achsyncnormalmode').append('<i id="SyncAch2" onclick="SendSyncMessage()" class="fas fa-sync achievementsettings" title="Click to push changes right now"></i>');
+					loadAchievements();
+				}
+				else
+					loadAchievementsEditMode();
+				return;
+			}
+
+			if (dataArray[dataPlayerID].split(":")[1].includes(',' + "" + achievementID + ',')) { //DETECT EXISTING ACHIEVEMENT, REMOVE IT
+				let toReplace = achievementID + ",";//REPLACE FROM WITHIN DATA
+				dataArrayPlayer = dataArray[dataPlayerID].split(":")[0] + ":" + dataArray[dataPlayerID].split(":")[1].replace(toReplace, "");
+				dataArray[dataPlayerID] = dataArrayPlayer;
+				toSYNC = dataArray.join("||");
+				//console.log(toSYNC);
+			}
+			else if (dataArray[dataPlayerID].split(":")[1].split(",")[0] == "" + achievementID) { //FIRST ACHIEVEMENT IN DATA?
+				let toReplace = achievementID + ",";//REPLACE FIRST ENTRY IN DATA
+				var firstDataArray = dataArray[dataPlayerID].split(":")[1].split(",");
+				firstDataArray.shift();
+				dataArray[dataPlayerID] = dataArray[dataPlayerID].split(":")[0] + ":" + firstDataArray;
+				toSYNC = dataArray.join("||");
+				//console.log(toSYNC);
+			}
+			else if (dataArray[dataPlayerID].split(":")[1].split(",")[dataArray[dataPlayerID].split(":")[1].split(",")[0].length + 1] == "" + achievementID) { //LAST ACHIEVEMENT IN DATA?
+				let toReplace = achievementID + ",";//REPLACE FIRST ENTRY IN DATA
+				var firstDataArray = dataArray[dataPlayerID].split(":")[1].split(",");
+				firstDataArray.pop();
+				dataArray[dataPlayerID] = dataArray[dataPlayerID].split(":")[0] + ":" + firstDataArray;
+				toSYNC = dataArray.join("||");
+				//console.log(toSYNC);
+			}
+			if (document.getElementById('SyncAchUnsaved') != null) {
+				if (document.getElementById('SyncAchUnsaved').value == toSYNC) {
+					document.getElementById('SyncAchUnsaved').id = "SyncAch";
+					document.getElementById('SyncAch').value = "";
+				}
+			}
+			if (document.getElementById('SyncAch') != null) {
+				document.getElementById('SyncAch').value = toSYNC;
+				document.getElementById('SyncAch').id = "SyncAchUnsaved";
+			}
+
+			await game.settings.set('farchievements', 'clientdataSYNC', toSYNC);
+			SendSyncMessage();
+			//RELOAD ANY OPEN WINDOW
+			if(document.getElementById('AchPlayerNav') == null) return;
+			if (document.getElementById('AchPlayerNav').className == "AchPlayerNav") //CHECK FOR EDITING WITHIN NORMAL WINDOW
+			{
+				await game.settings.set('farchievements', 'loadSettingsForPlayer', PID);
+				$('#achsyncnormalmode').append('<i id="SyncAch2" onclick="SendSyncMessage()" class="fas fa-sync achievementsettings" title="Click to push changes right now"></i>');
+				window.loadAchievements();
+			}
+			else
+				window.loadAchievementsEditMode();
+}
+
+async function SendSyncMessage() {
+	ChatMessage.create({
+		user: game.user._id,
+		content: "Achievements Synced",
+		blind: false,
+		whisper: game.users.filter(u => u.isGM).map(u => u.id)
+	});
+
+	if(document.getElementById('achsyncnormalmode') != null)
+		if (document.getElementById('achsyncnormalmode').innerHTML != "") 
+			document.getElementById('achsyncnormalmode').innerHTML = "";
+}
